b3bd67fedef14fd34f5daf1e4b31420932269810
abtn <digitalexb2tn@gmail.com>
2023-11-13 13:38:06 +0100

    Last modif
    Dernières modifications avant mise en ligne

--- js/menumota.js
+++ js/menumota.js
@@ -18,18 +18,22 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 //Affichage menu lors du scroll
-document.addEventListener('DOMContentLoaded', function() {
-    var lastScrollTop = 0; 
-    var barmenu = document.getElementById('barmenu'); 
+document.addEventListener("DOMContentLoaded", function() {
+    var lastScrollTop = 0;
+    var navbar = document.getElementById("barmenu");
 
-    window.addEventListener('scroll', function() {
+    window.addEventListener("scroll", function() {
         var currentScroll = window.scrollY || document.documentElement.scrollTop;
 
-        if (currentScroll > lastScrollTop) {
-            barmenu.style.top = '-100px'; 
+        if (currentScroll > lastScrollTop && currentScroll > 80) {
+            // Scrolling Down
+            if (!navbar.classList.contains("is-active")) {
+                navbar.style.top = "-100px";
+            }
         } else {
-            barmenu.style.top = '0px';
+            // Scrolling Up
+            navbar.style.top = "0px";
         }
-        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; //mobile
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
     }, false);
 });
